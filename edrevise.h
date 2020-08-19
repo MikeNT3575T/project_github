3b55463c387e3ea22002d42cadfd17dce31aa4bc
yehtony <yehtony@TWPCNT3574T.ZyXEL.com>
2020-08-19 14:46:13 +0800

    修正版

---
+++ edrevise.h
@@ -0,0 +1,21 @@
+#ifndef BUF_SIZE
+#define BUF_SIZE 512
+#include <iostream>
+using namespace std;
+extern char buf[];
+extern wstring date;
+extern wstring time;
+extern wstring apname;
+extern wstring apmac;
+extern wstring stamac;
+extern wstring mesg;
+extern wstring ssid;
+extern wstring connection;
+extern wstring band;
+extern wstring signal;
+extern int count;
+void InitializeSocket();
+void BlindSocket(int p);
+void AcceptData();
+void ReleaseSocket(void);
+#endif
